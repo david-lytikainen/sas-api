b7934a79af54df4f5cb014e83e79c806b73f3400
David Lytikainen <davidlyt@hey-brotha.local>
2025-03-15 12:24:55 -0400

    flask db init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +9,7 @@ load_dotenv()
 
 # Initialize extensions
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -17,7 +19,8 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     # Initialize Flask extensions
-    db.init_app(app)
-    
+    db.init_app(app) # initialize sqlalchemy
+    migrate.init_app(app, db) # initialize flask-migrate
+
     return app
     
