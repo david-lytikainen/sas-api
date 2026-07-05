503f7f869f3d4c4490e964210fba477192ff40ca
agentbot <agentbot>
2026-07-05 04:02:12 +0000

    Simplify scheduler email cleanup

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import os
-from importlib import import_module
 from dotenv import load_dotenv
 from app import create_app
 from app.scheduler import start_embedded_scheduler
@@ -11,7 +10,6 @@ load_dotenv()
 # Create the Flask application
 app = create_app()
 start_embedded_scheduler(app)
-import_module("app.models.event_waitlist")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
